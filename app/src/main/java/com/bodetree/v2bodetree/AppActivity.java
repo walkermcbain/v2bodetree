523171f0b161bcf192db13316a8c2fed222d1a4e
walkermcbain <walkermcbain@gmail.com>
2017-07-28 12:43:30 +0100

    Initial commit

---
+++ app/src/main/java/com/bodetree/v2bodetree/AppActivity.java
@@ -0,0 +1,134 @@
+package com.bodetree.v2bodetree;
+
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.app.admin.SystemUpdatePolicy;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//Friday work done/to do
+//solved stop lock..app crashes when pressed "unfortunately v2bodetree has stopped"...
+//solve unlockApp..with pin
+//disabled back and home buttons..codelab
+//add one view..app crashed when pressed "same as above"
+
+
+public class AppActivity extends AppCompatActivity {
+
+    private ComponentName mAdminComponentName;
+    private DevicePolicyManager mDevicePolicyManager;
+    private Button next_screen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+
+// Retrieve Device Policy Manager so that we can check whether we can
+// lock to screen later
+        mAdminComponentName = new ComponentName(this,AppAdminReceiver.class);
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        if(mDevicePolicyManager.isDeviceOwnerApp(getPackageName())){
+            // App is whitelisted
+            setDefaultCosuPolicies(true);
+        }
+        else {
+
+        }
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+// Consider locking your app here or by some other mechanism
+// Active Manager is supported on Android M
+        if(mDevicePolicyManager.isLockTaskPermitted(this.getPackageName())){
+            ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            if (am.getLockTaskModeState() == ActivityManager.LOCK_TASK_MODE_NONE) {
+                setDefaultCosuPolicies(true);
+                startLockTask();
+            }
+        }
+    }
+
+    //button action goes here, need a better method
+    public void onButtonClicked(View view) {
+        Intent intent = new Intent(this, view2.class);
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+
+
+    private void setDefaultCosuPolicies(boolean active){
+
+        // Set user restrictions
+        setUserRestriction(UserManager.DISALLOW_SAFE_BOOT, active);
+        setUserRestriction(UserManager.DISALLOW_FACTORY_RESET, active);
+        setUserRestriction(UserManager.DISALLOW_ADD_USER, active);
+        setUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA, active);
+        setUserRestriction(UserManager.DISALLOW_ADJUST_VOLUME, active);
+
+        // Disable keyguard and status bar
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, active);
+        mDevicePolicyManager.setStatusBarDisabled(mAdminComponentName, active);
+
+        // Enable STAY_ON_WHILE_PLUGGED_IN
+        enableStayOnWhilePluggedIn(active);
+
+        // Set system update policy
+        if (active){
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName, SystemUpdatePolicy.createWindowedInstallPolicy(60, 120));
+        } else {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName,null);
+        }
+
+        // set this Activity as a lock task package
+        mDevicePolicyManager.setLockTaskPackages(mAdminComponentName,active ? new String[]{getPackageName()} : new String[]{});
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_MAIN);
+        intentFilter.addCategory(Intent.CATEGORY_HOME);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        if (active) {
+            // set Cosu activity as home intent receiver so that it is started
+            // on reboot
+            mDevicePolicyManager.addPersistentPreferredActivity(mAdminComponentName, intentFilter, new ComponentName(getPackageName(), AppActivity.class.getName()));
+        } else {
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(mAdminComponentName, getPackageName());
+        }
+    }
+
+    private void setUserRestriction(String restriction, boolean disallow){
+        if (disallow) {
+            mDevicePolicyManager.addUserRestriction(mAdminComponentName,restriction);
+        } else {
+            mDevicePolicyManager.clearUserRestriction(mAdminComponentName,restriction);
+        }
+    }
+
+    private void enableStayOnWhilePluggedIn(boolean enabled){
+        if (enabled) {
+            mDevicePolicyManager.setGlobalSetting(mAdminComponentName, Settings.Global.STAY_ON_WHILE_PLUGGED_IN,Integer.toString(BatteryManager.BATTERY_PLUGGED_AC| BatteryManager.BATTERY_PLUGGED_USB| BatteryManager.BATTERY_PLUGGED_WIRELESS));
+        } else {
+            mDevicePolicyManager.setGlobalSetting(mAdminComponentName,Settings.Global.STAY_ON_WHILE_PLUGGED_IN,"0");
+        }
+    }
+}
+
---
+++ app/src/main/java/com/bodetree/v2bodetree/view2.java
@@ -0,0 +1,20 @@
+package com.bodetree.v2bodetree;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by walkermcbain on 7/28/17.
+ */
+
+public class view2 extends AppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view2);
+    }
+
+
+}
