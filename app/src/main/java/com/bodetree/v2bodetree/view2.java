51a420407ac3ca0806853b7f239335c0b7632520
walkermcbain <walkermcbain@gmail.com>
2017-08-23 19:32:01 -0400

    video plays, images in

--- app/src/main/java/com/bodetree/v2bodetree/view2.java
+++ app/src/main/java/com/bodetree/v2bodetree/view2.java
@@ -1,42 +1,106 @@
 package com.bodetree.v2bodetree;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.MediaController;
+import android.app.ProgressDialog;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.util.Log;
 import android.widget.VideoView;
 
 
 
-public class view2 extends AppCompatActivity{
 
-    VideoView videoView;
-    DisplayMetrics dm;
-    SurfaceView sur_View;
-    MediaController media_Controller;
+
+public class view2 extends AppCompatActivity {
+
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view2);
+        //set the media controller buttons
+
+        if (mediaControls == null) {
+            mediaControls = new MediaController(view2.this);
+        }
+
+        //initialize the VideoView
+        myVideoView = (VideoView) findViewById(R.id.videoView3);
+
+        // create a progress bar while the video file is loading
+        progressDialog = new ProgressDialog(view2.this);
+
+        // set a title for the progress bar
+        progressDialog.setTitle("City Wide");
+
+        // set a message for the progress bar
+        progressDialog.setMessage("Loading...");
+
+        //set the progress bar not cancelable on users' touch
+        progressDialog.setCancelable(false);
+        // show the progress bar
+        progressDialog.show();
+
+        try {
+            //set the media controller in the VideoView
+            myVideoView.setMediaController(mediaControls);
+            //set the uri of the video to be played
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.jeffoddo));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+
+        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+        myVideoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // close the progress bar and play the video
+                progressDialog.dismiss();
+                //if we have a position on savedInstanceState, the video playback should start from here
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    //if we come from a resumed activity, video playback will be paused
+                    myVideoView.pause();
+                }
+            }
+        });
+
     }
 
-    public void getInit(){
-        videoView = (VideoView) findViewById(R.id.videoView);
-        media_Controller = new MediaController(this);
-        dm = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int height = dm.heightPixels;
-        int width = dm.widthPixels;
-        videoView.setMinimumWidth(width);
-        videoView.setMinimumHeight(height);
-        videoView.setMediaController(media_Controller);
-        videoView.setVideoPath("/res/raw/jeffoddo.mp4");
-        videoView.start();
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        //we use onSaveInstanceState in order to store the video playback position for orientation change
+        savedInstanceState.putInt("Position", myVideoView.getCurrentPosition());
+        myVideoView.pause();
     }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        //we use onRestoreInstanceState in order to play the video playback from the stored position
+        position = savedInstanceState.getInt("Position");
+        myVideoView.seekTo(position);
+    }
+
+
+
+
 
     public void onButtonClicked(View view){
         Intent intent = new Intent(this, view3.class);
@@ -48,5 +112,4 @@ public class view2 extends AppCompatActivity{
     public void onBackPressed() {
 
     }
-
 }
