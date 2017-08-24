092ece91965b8ac645256e7724b1d50c623e9466
walkermcbain <walkermcbain@gmail.com>
2017-08-24 01:22:46 +0100

    video plays, images in, pdfs in

--- app/src/main/java/com/bodetree/v2bodetree/view2.java
+++ app/src/main/java/com/bodetree/v2bodetree/view2.java
@@ -27,33 +27,29 @@ public class view2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view2);
-        //set the media controller buttons
 
         if (mediaControls == null) {
             mediaControls = new MediaController(view2.this);
         }
 
-        //initialize the VideoView
         myVideoView = (VideoView) findViewById(R.id.videoView3);
 
-        // create a progress bar while the video file is loading
+
         progressDialog = new ProgressDialog(view2.this);
 
-        // set a title for the progress bar
+
         progressDialog.setTitle("City Wide");
 
-        // set a message for the progress bar
+
         progressDialog.setMessage("Loading...");
 
-        //set the progress bar not cancelable on users' touch
+
         progressDialog.setCancelable(false);
-        // show the progress bar
+
         progressDialog.show();
 
         try {
-            //set the media controller in the VideoView
             myVideoView.setMediaController(mediaControls);
-            //set the uri of the video to be played
             myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.jeffoddo));
 
         } catch (Exception e) {
@@ -63,18 +59,16 @@ public class view2 extends AppCompatActivity {
 
         myVideoView.requestFocus();
 
-        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+
         myVideoView.setOnPreparedListener(new OnPreparedListener() {
 
             public void onPrepared(MediaPlayer mediaPlayer) {
-                // close the progress bar and play the video
+
                 progressDialog.dismiss();
-                //if we have a position on savedInstanceState, the video playback should start from here
                 myVideoView.seekTo(position);
                 if (position == 0) {
                     myVideoView.start();
                 } else {
-                    //if we come from a resumed activity, video playback will be paused
                     myVideoView.pause();
                 }
             }
@@ -85,7 +79,6 @@ public class view2 extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        //we use onSaveInstanceState in order to store the video playback position for orientation change
         savedInstanceState.putInt("Position", myVideoView.getCurrentPosition());
         myVideoView.pause();
     }
@@ -93,13 +86,9 @@ public class view2 extends AppCompatActivity {
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        //we use onRestoreInstanceState in order to play the video playback from the stored position
         position = savedInstanceState.getInt("Position");
         myVideoView.seekTo(position);
     }
-
-
-
 
 
     public void onButtonClicked(View view){
--- app/src/main/java/com/bodetree/v2bodetree/view4.java
+++ app/src/main/java/com/bodetree/v2bodetree/view4.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.github.barteksc.pdfviewer.PDFView;
 
 
 public class view4 extends AppCompatActivity{
 
+    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view4);
+
+        pdfView = (PDFView)findViewById(R.id.pdfView);
+        pdfView.fromAsset("City Wide 2017 MULTI STATE Unit FDD v2.pdf").load();
 
 
     }
